0d2a805077803cd2c4df6faef2c947eb35b02b8d
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2021-01-24 00:21:05 +0100

    The software is working

--- slim-lint-report.rb
+++ slim-lint-report.rb
@@ -3,4 +3,28 @@
 require 'fileutils'
 require 'pathname'
 
-puts "hello world"
+linter = "bundle exec slim-lint source/**/*.slim"
+lint_count = 0 
+
+output = %x[ #{linter} ]
+
+paths = []
+output.each_line do |line|
+  path = line.split(":")[0]
+  paths << path
+  lint_count += 1 
+end
+
+grouped = Hash.new(0)
+paths.each do |line|
+  grouped[line] += 1
+end
+
+grouped.each do |k, v|
+  puts "#{v} — #{k}"
+end
+
+puts "\n\n"
+puts "------------------------------------------\n"
+puts "------------------------------------------\n"
+puts "You have #{lint_count} warnings."
